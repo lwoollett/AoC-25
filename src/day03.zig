f437c7b0f7ac249300ae3ecaea6c933f9de2cac7
Luke Woollett <woollettluke@gmail.com>
2025-12-06 20:55:22 +1300

    [feat] day3 p2 && day 3 cleanup to use a single helper func

--- src/day03.zig
+++ src/day03.zig
@@ -14,53 +14,50 @@ const numWithIdx = struct {
     num: i8,
     index: i8,
 };
+
 pub fn main() !void {
-    var batterycapacity: i64 = 0;
+    var batterycapacityp1: i64 = 0;
+    var batterycapacityp2: i64 = 0;
     var lines = std.mem.tokenizeAny(u8, data, "\r\n");
     while (lines.next()) |line| {
         // Convert line to []i8
-        batterycapacity += try getVoltageForLine(line);
+        batterycapacityp1 += try getVoltageForLine(line, 2);
+        batterycapacityp2 += try getVoltageForLine(line, 12);
     }
-    print("Part 1 Result: {d}\n", .{batterycapacity});
+    print("Part 1 Result: {d}\n", .{batterycapacityp1});
+    print("Part 2 Result: {d}\n", .{batterycapacityp2});
 }
 
-fn getVoltageForLine(line: []const u8) !i64 {
-    // ! TODO: Optimise alloc (probably less than 64 on input)
-    // ! Zig 15 ArrayList allocator is weird
-    // ! Docs also aren't updated lmao
-    var nums = try List(i8).initCapacity(gpa, 128);
-    defer nums.deinit(gpa);
-    // Each char in input is a digit
-    for (line) |c| {
-        const digit = @as(i8, @intCast(c - '0'));
-        try nums.append(gpa, digit);
-    }
-    // !! We're not allowed to sort the input array
-    // So we have to find the top two digits manually
-    var first_max: numWithIdx = .{ .num = -1, .index = -1 };
-    var second_max: numWithIdx = .{ .num = -1, .index = -1 };
-    // We want to optimise to get the highest number possible as the first, with the second number being a lesser prize
-    // Second_max has to be AFTER first_max in the input list
-    // Do two passes to find first and second max
-    // First max has to have at least one number after it
-    var idx: i8 = 0;
-    for (nums.items[0 .. nums.items.len - 1]) |n| {
-        if (n > first_max.num) {
-            first_max = .{ .num = n, .index = @as(i8, idx) };
+fn getVoltageForLine(line: []const u8, keep: i8) !i64 {
+
+    // Use ArrayList(u8) for stack of digits (as integers 0-9)
+    var stack = try std.ArrayList(u8).initCapacity(gpa, 128);
+    defer stack.deinit(gpa);
+
+    const len = line.len;
+    for (line, 0..) |c, i| {
+        const digit = c - '0'; // ASCII to digit value
+        const remaining = len - i;
+
+        while (stack.items.len > 0 and
+            digit > stack.items[stack.items.len - 1] and
+            stack.items.len + (remaining - 1) >= keep)
+        {
+            _ = stack.pop();
         }
-        idx += 1;
+
+        try stack.append(gpa, digit);
     }
-    idx = 0;
-    for (nums.items[@as(usize, @intCast(first_max.index + 1))..]) |n| {
-        if (n > second_max.num) {
-            second_max = .{ .num = n, .index = @as(i8, idx) };
-        }
-        idx += 1;
+
+    const selected = stack.items[0..@as(usize, @intCast(keep))];
+
+    // Parse to i64
+    var result: i64 = 0;
+    for (selected) |digit| {
+        result = result * 10 + digit;
     }
-    print("Top two digits: {d}, {d}\n", .{ first_max.num, second_max.num });
-    // Instead of doing weird string shit just x10 the first val
-    return (first_max.num * 10) + second_max.num;
-    // * We're gucci gaming
+
+    return result;
 }
 
 // Useful stdlib functions
